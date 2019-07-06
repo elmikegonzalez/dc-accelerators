b75326ee5ae2a160971bc6e887adef68e90cf60b
Miguel Gonzalez <miguel.gonzalez@gmail.com>
2019-07-06 15:20:45 -0700

    cleaned build batch

--- build.sh
+++ build.sh
@@ -4,8 +4,3 @@ echo npm install
 npm install --verbose
 echo building...
 gulp buildAllWithoutReload
-#echo uploading to s3...
-#gulp upload-content-types
-#echo uploading icons to s3
-#gulp upload-icons
-#echo uploading viz
